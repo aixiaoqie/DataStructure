d2c835009742612d7322f88cdacbd990445a56cd
aixiaoqie <2390734879@qq.com>
2020-12-11 23:21:58 +0800

    堆排coding

--- src/com/ssw/sorted/TestSorted.java
+++ src/com/ssw/sorted/TestSorted.java
@@ -1,14 +1,18 @@
 package com.ssw.sorted;
 
 
+import src.com.ssw.sorted.HeapSorted;
+
 public class TestSorted {
 
     public static void main(String[] args) {
         int[] a = {1, 4, 8, 5, 3, 9, 5, 3, 3};
-        QuickSorted.quickSorted(a);
-//        BubbleSorted.bubbleSorted2(a);
+//        QuickSorted.quickSorted(a);
+//        BubbleSorted.bubbleSorted2(
+//        a);
 //        InsertSorted.insertSorted(a);
 //        SelectSorted.selectSorted(a);
+        HeapSorted.heapSorted(a);
         for (int i = 0; i < a.length; i++) {
             System.out.println(a[i]);
         }
