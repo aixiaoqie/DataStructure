9d735a6c3bd654a2f782f3df75525ae39aecd03e
aixiaoqie <2390734879@qq.com>
2020-12-15 21:45:27 +0800

    链表特殊删除coding

---
+++ src/com/ssw/linkedlist/topic/RemoveNodewithoutHead.java
@@ -0,0 +1,31 @@
+package src.com.ssw.linkedlist.topic;
+
+/**
+ * 一个值为int类型的链表，不给链表head节点，如何删除一个指定的节点，
+ * 时间复杂度为O(1)，并且分析可能出现的问题
+ */
+public class RemoveNodewithoutHead {
+
+    class Node {
+        public int value;
+        public Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public void removeNode(Node node) throws Exception {
+        if (node == null) {
+            return;
+        }
+
+        Node next = node.next;
+        if (next == null) {
+            throw new Exception("无法删除最后一个节点");
+        }
+        node.value = next.value;  //保留下一个节点的值到当前节点，并删除下个节点
+        node.next = next.next;
+    }
+
+}
