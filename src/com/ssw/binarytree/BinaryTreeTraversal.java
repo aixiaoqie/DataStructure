f44553041050f279ef7eb41c367b6060d3bf6592
aixiaoqie <2390734879@qq.com>
2020-12-16 23:23:56 +0800

    递归实现二叉树遍历

---
+++ src/com/ssw/binarytree/BinaryTreeTraversal.java
@@ -0,0 +1,47 @@
+package src.com.ssw.binarytree;
+
+/**
+ * 二叉树遍历
+ */
+public class BinaryTreeTraversal {
+
+    class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    //递归实现先序遍历
+    public void preRootTraversal(Node head) {
+        if (head == null) {
+            return;
+        }
+        System.out.println(head.value + "");
+        preRootTraversal(head.left);
+        preRootTraversal(head.right);
+    }
+
+    //递归实现中序遍历
+    public void midRootTraversal(Node head) {
+        if (head == null) {
+            return;
+        }
+        midRootTraversal(head.left);
+        System.out.println(head.value + "");
+        midRootTraversal(head.right);
+    }
+
+    //递归实现后序遍历
+    public void afterRootTraversal(Node head) {
+        if (head == null) {
+            return;
+        }
+        afterRootTraversal(head.left);
+        afterRootTraversal(head.right);
+        System.out.println(head.value + "");
+    }
+}
