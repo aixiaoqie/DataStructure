9c952e61ab0abec7b010949245760abe406845c7
aixiaoqie <2390734879@qq.com>
2020-12-20 22:49:58 +0800

    较为直观的打印一颗树，并进行main方法验证

--- src/com/ssw/binarytree/PrettyPrintTree.java
+++ src/com/ssw/binarytree/PrettyPrintTree.java
@@ -4,7 +4,7 @@ package src.com.ssw.binarytree;
  * 较为直观的打印一颗树，方便构造其结构
  */
 public class PrettyPrintTree {
-    class Node {
+    static class Node {
         public int value;
         public Node left;
         public Node right;
@@ -12,6 +12,21 @@ public class PrettyPrintTree {
         public Node(int value) {
             this.value = value;
         }
+    }
+
+    public static void main(String[] args) {
+        PrettyPrintTree.Node head = new PrettyPrintTree.Node(1);
+        Node left1_1 = new Node(2);
+        Node right1_1 = new Node(3);
+        head.left = left1_1;
+        head.right = right1_1;
+        Node right2_4 = new Node(5);
+        Node left2_1 = new Node(100);
+        left1_1.left = left2_1;
+        right1_1.right = right2_4;
+
+        PrettyPrintTree prettyPrintTree = new PrettyPrintTree();
+        prettyPrintTree.printTree(head);
     }
 
 
@@ -24,6 +39,12 @@ public class PrettyPrintTree {
     /**
      * @param head   头节点
      * @param height 一个二叉树逆时针90度 从左到右打印为向上的高度
+     *               大体示意图如下
+     *                                          V5V       
+     *                         V3V
+     *        H1H
+     *                         ∧2∧
+     *                                         ∧100∧
      * @param to     插入的特殊字符
      * @param len    打印的每行长度
      */
@@ -31,14 +52,14 @@ public class PrettyPrintTree {
         if (head == null) {
             return;
         }
-        printInOrder(head.right, height + 1, "v", len);
+        printInOrder(head.right, height + 1, "V", len);
         String val = to + head.value + to;
         int lenM = val.length();//32位节点的值
         int lenL = (len - lenM) / 2;//左面需要补全空格的长度
         int lenR = len - lenM - lenL;//右面需要补全空格的长度
         val = getSpace(lenL) + val + getSpace(lenR); //生成17位长度的打印统一规范
-        System.out.println(getSpace(height * len) + val);
-        printInOrder(head.left, height + 1, "M", len);
+        System.out.println(getSpace(height * len) + val); //第一行先打印树的最有子树的右节点，
+        printInOrder(head.left, height + 1, "∧", len);
     }
 
 
