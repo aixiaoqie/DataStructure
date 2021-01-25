834e0e9582554955d43ccac96aa585194d56138a
aixiaoqie <2390734879@qq.com>
2021-01-25 20:41:46 +0800

    自然数数组的排序

---
+++ src/com/ssw/arrays/topic/NaturalNumSort.java
@@ -0,0 +1,11 @@
+package com.ssw.arrays.topic;
+
+/**
+ * 自然数数组的排序
+ * <p>
+ * 题目：给定一个长度为N的整型数组arr，其中有N个互不相等的自然数1~N,请实现arr的排序，但不要把下标0~N-1位置上的数直接通过赋值的方式替换
+ * <p>
+ * 要求：时间复杂度O(1),额外空间复杂度O(1).
+ */
+public class NaturalNumSort {
+}
