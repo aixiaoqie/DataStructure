037944a86eef275f974c6590b2c6dac7707fa938
aixiaoqie <2390734879@qq.com>
2021-02-12 23:33:26 +0800

    test

---
+++ src/com/ssw/Test.java
@@ -0,0 +1,25 @@
+package com.ssw;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        String s = "rat";
+        String t = "tar";
+        System.out.println(isAnagram(s,t));
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+        Arrays.sort(sArr);
+        Arrays.sort(tArr);
+        String s1 = String.valueOf(sArr);
+        String s2 = String.valueOf(tArr);
+        return s1.equals(s2);
+    }
+}
